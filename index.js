c66384bfe8077588cc7bcec6d9aa42ce31eabe01
unmadebed <kevin.archer@live.com>
2021-08-17 23:17:38 +0100

    toggle message and link card visibility

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.querySelector('form').addEventListener('submit', (event) => {
 	event.preventDefault();
 
-	const input = document.querySelector('#message-input');
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#link-form').classList.remove('hide');
 
+	const input = document.querySelector('#message-input');
 	const encrypted = btoa(input.value);
 
 	const linkInput = document.querySelector('#link-input');
